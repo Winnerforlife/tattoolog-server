764b93a83f143a567fe865ea96849bc0dfc6ad81
Stanislav <stanislav@macbook-pro-2.home>
2023-09-26 18:41:35 +0200

    Added funcionality in signals for adding social media for new users

--- apps/accounts/signals.py
+++ apps/accounts/signals.py
@@ -2,9 +2,15 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from apps.accounts.models import Profile, CustomUser
+from apps.tools.models import SocialMediaType, SocialMedia
 
 
 @receiver(post_save, sender=CustomUser)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        profile = Profile.objects.create(user=instance)
+        existing_media_types = SocialMediaType.objects.all()
+
+        for media_type in existing_media_types:
+            SocialMedia.objects.create(profile=profile, social_media_type=media_type)
+
