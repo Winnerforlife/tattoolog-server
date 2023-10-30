049b6996a2188972abed044f1bb7f637dc8d0e8d
Stanislav <stanislav@macbook-pro-2.home>
2023-10-30 18:17:15 +0100

    try settings s3

--- config/settings.py
+++ config/settings.py
@@ -130,8 +130,10 @@ if public_key and private_key and bucket:
 
     STATIC_URL = 'https://%s/static/' % AWS_S3_CUSTOM_DOMAIN
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    MEDIA_URL = 'https://%s/media/' % AWS_S3_CUSTOM_DOMAIN
+    STATIC_ROOT = 'config/static/'
+    MEDIA_URL = 'https://%s/uploads/' % AWS_S3_CUSTOM_DOMAIN
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    MEDIA_ROOT = 'config/uploads/'
 else:
     STATIC_URL = '/config/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'config/static/')
