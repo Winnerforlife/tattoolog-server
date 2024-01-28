ffc32e729ddf89278fcea51fd17dea2915c780df
Stanislav <stanislav@macbook-pro-2.home>
2024-01-28 16:42:11 +0100

    fix: Extended data in festival-list for field category

--- apps/tools/serializers.py
+++ apps/tools/serializers.py
@@ -63,6 +63,8 @@ class FestivalCategorySerializer(serializers.ModelSerializer):
 
 
 class FestivalSerializer(serializers.ModelSerializer):
+    category = FestivalCategorySerializer(read_only=True)
+
     class Meta:
         model = Festival
         fields = (
